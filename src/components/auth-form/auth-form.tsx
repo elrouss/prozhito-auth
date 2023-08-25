41d83556aae59625ea0ac3c4a1de7b62d7265256
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-08-25 12:10:27 +0300

    feat: add btn component

--- src/components/auth-form/auth-form.tsx
+++ src/components/auth-form/auth-form.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { DefaultButton } from '@/shared/buttons/default-btn/default-btn';
 import styles from './auth-form.module.scss';
 
-type TAuthFormProps = {
+interface IAuthFormProps extends React.HTMLAttributes<HTMLFormElement> {
   children: React.ReactElement;
   heading: string;
-};
+  buttonLabel: string;
+  isLoading?: boolean;
+}
 
-export const AuthForm = ({ children, heading }: TAuthFormProps) => (
+export const AuthForm = ({
+  children,
+  heading,
+  buttonLabel,
+  isLoading = false,
+  onSubmit,
+}: IAuthFormProps) => (
   <div className={styles.wrapper}>
     <h1>{heading}</h1>
-    <form className={styles.form}>{children}</form>
+    <form className={styles.form} onSubmit={onSubmit}>
+      {children}
+      <DefaultButton label={buttonLabel} isLoading={isLoading} />
+    </form>
   </div>
 );
---
+++ src/shared/buttons/default-btn/default-btn.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import clsx from 'clsx';
+import styles from './default-btn.module.scss';
+
+interface IDefaultButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  label: string;
+  type?: 'button' | 'submit' | 'reset';
+  isLoading?: boolean;
+}
+
+export const DefaultButton = ({
+  label,
+  type = 'button',
+  isLoading = false,
+}: IDefaultButtonProps) => (
+  <button
+    className={clsx(styles.button, { [styles.load]: isLoading })}
+    type={type}
+  >
+    {label}
+  </button>
+);
